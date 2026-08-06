317fb92a26a660033a4b44c680789dede3cb0515
Aprianita <aprianita.rpl1@gmail.com>
2026-08-06 13:23:59 +0700

    Menambahkan program while penjualan stok

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  String namaBarang = "Buku Tulis";
-  int hargaAnggota = 300;
-  int hargaUmum = 3500;
-  int jualanStok = 40;
-  bool tersedia = true;
+  int stok = 3;
 
-  int jumlahBeli = 3;
+  print("--- Penjualan Buku Tulis ---");
 
-  int totalAnggota = jumlahBeli * hargaAnggota;
-  int totalUmum = jumlahBeli * hargaUmum;
-  int selisih = totalUmum * totalAnggota;
-
-  print ("=== KARTU DATA BARANG ===");
-  print ("Nama Barang : $namaBarang");
-  print ("harga Anggota : Rp $hargaAnggota");
-  print ("harga Umum : Rp $hargaUmum");
-  print ("Jumlah Stok : $jualanStok");
-  print ("tersedia : $tersedia");
-
-  runApp(const MyApp());
+  while (stok > 0) {
+    stok--;
+    print("Terjual 1, sisa stok: $stok");
   }
 
-  class MyApp extends StatelessWidget {
-    const MyApp({super.key});
-
-    @override
-    Widget build(BuildContext context) {
-      return MaterialApp(
-        home:Scaffold(
-          appBar: AppBar(title: const Text("Data Barang")),
-        body: const Center(
-          child: Text("Hasil print di Debug Console"),
-          ),
-        ),
-      );
-    }
-  }
-// Pemilihan tipe data yang tepat membuat perhitungan kasir menjadi akurat.
-// Harga dan stok menggunakan tipe data angka agar dapat dihitung dengan benar,
-// sedangkan status tersedia menggunakan boolean karena hanya memiliki dua kondisi: true atau false.
+  // Bahaya jika kondisi while keliru adalah perulangan bisa berjalan terus
+  // atau stok menjadi negatif sehingga penjualan melebihi stok yang tersedia.
+  // Cara mencegahnya adalah menggunakan kondisi yang benar (stok > 0)
+  // dan mengurangi stok setiap kali terjadi penjualan.
+}
