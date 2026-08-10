8d75a447c501ba54e6c1224d337c7571a2c8a32e
Aprianita <aprianita.rpl1@gmail.com>
2026-08-10 09:34:14 +0700

    List Barang

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,40 @@
 import "package:flutter/material.dart";
 
-class Barang{
+class Barang {
   String nama;
   double harga;
   int stok;
 
   Barang(this.nama, this.harga, this.stok);
 
-  void tampilkanInfo(){
+  void tampilkanInfo() {
     print("===================================");
-    print("KARTU BARANG ");
+    print("KARTU BARANG");
     print("Nama : $nama");
     print("Harga : Rp${harga.toStringAsFixed(0)}");
     print("Stok : $stok");
     print("===================================");
   }
 }
-void main(){
+
+void main() {
   Barang barang1 = Barang("Buku Tulis", 3000, 20);
   Barang barang2 = Barang("Pulpen", 2500, 15);
   Barang barang3 = Barang("Roti", 5000, 10);
 
-  barang1.tampilkanInfo();
-  barang2.tampilkanInfo();
-  barang3.tampilkanInfo();
+  List<Barang> daftarBarang = [barang1, barang2, barang3];
+
+  for (Barang barang in daftarBarang) {
+    barang.tampilkanInfo();
+  }
+
+  /*
+  Perbandingan dengan Sprint 3:
+  Pada Sprint 3, data nama dan harga barang disimpan secara terpisah
+  menggunakan dua List, yaitu List<String> namaBarang dan List harga.
+  Pada sprint sekarang, data nama, harga, dan stok digabung menjadi
+  satu objek Barang. Kemudian beberapa objek Barang disimpan dalam
+  satu List<Barang>, sehingga data menjadi lebih terorganisir,
+  saling berhubungan, dan lebih mudah untuk digunakan.
+  */
 }
